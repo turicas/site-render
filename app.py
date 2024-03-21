d11b7135263d73b8babe678513574db08ee66d99
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2024-03-20 22:22:29 -0300

    new code

--- app.py
+++ app.py
@@ -1,66 +1,24 @@
-import re
-import unicodedata
-
+import os
 import requests
-from flask import Flask
-from lxml.html import document_fromstring
-
-
-def normaliza(texto):
-  "Normaliza um texto retirando acentos, caracteres especiais e espaços desnecessários"
-  texto = texto.lower()
-  texto = unicodedata.normalize("NFKD", texto).encode("ascii", errors="ignore").decode("ascii")
-  texto = re.sub(r"[^\w\s]", "", texto)
-  texto = re.sub(r"\s+", " ", texto)
-  return texto.strip()
-
-
-def materias_home_estadao():
-  "Retorna matérias encontradas na home do Estadão"
-  response = requests.get("https://www.estadao.com/")
-  tree = document_fromstring(response.content)
-  tree.xpath("//a[contains(//h2/@class, 'headline')]")
-  materias = tree.xpath("//a[contains(./h2/@class, 'headline')]")
-  resultado = []
-  for materia in materias:
-    texto = materia.xpath("./h2//text()")
-    if not texto:
-      continue
-    titulo = " ".join(texto).strip()
-    link = materia.xpath("./@href")[0]
-    resultado.append({"titulo": titulo, "url": link})
-  return resultado
+from flask import Flask, request
 
 
 app = Flask(__name__)
+BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 
 @app.route("/")
 def index():
-  return "Olá, <b>tudo bem</b>?"
+    return "Olá, <b>tudo bem</b>?"
 
+@app.route("/teste")
+def teste():
+    return "VOLTAMOS ÀS 21:30!"
 
-@app.route("/estadao")
-def estadao():
-    html = """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Ronda Estadão</title>
-    </head>
-    <body>
-        <h1>Ronda Estadão</h1>
-        <p>
-        As matérias encontradas foram:
-        <ul>
-    """
-    for materia in materias_home_estadao():
-        palavras = normaliza(materia["titulo"]).split(" ")
-        if "dengue" in palavras or "lula" in palavras or "bolsonaro" in palavras:
-            html += f'<li> <a href="{materia["url"]}">{materia["titulo"]}</a> </li>'
-    html += """
-        </ul>
-        </p>
-    </body>
-    </html>
-    """
-    return html
+@app.route("/telegram", methods=["POST"])
+def telegram_update():
+    update = request.json
+    url_envio_mensagem = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    chat_id = update["message"]["chat"]["id"]
+    mensagem = {"chat_id": chat_id, "text": "mensagem <b>recebida</b>!", "parse_mode": "HTML"}
+    requests.post(url_envio_mensagem, data=mensagem)
+    return "ok"
